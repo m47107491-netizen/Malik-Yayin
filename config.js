0e494920a747bcebfca6e8d8b69ee49442ce67d5
m47107491-netizen <m47107491@gmail.com>
2026-08-25 15:26:45 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+// Aynı origin (Vercel veya Railway). Boş bırak = kendi domainin.
+window.MALIKYAYIN_API_BASE = '';
+
+// Shopier ürün linkleri
+window.MALIKYAYIN_SHOPIER = {
+  7:   'https://www.shopier.com/malik__yayin/50221098',  // ₺30
+  30:  'https://www.shopier.com/malik__yayin/50221155',  // ₺70
+  60:  'https://www.shopier.com/malik__yayin/50221214',  // ₺120
+  365: 'https://www.shopier.com/malik__yayin/50221272'  // ₺500
+};
+
+window.MALIKYAYIN_RECAPTCHA_SITE_KEY = window.MALIKYAYIN_RECAPTCHA_SITE_KEY || '';
